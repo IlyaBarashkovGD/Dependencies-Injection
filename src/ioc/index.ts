f1351450a0c509c9bc682acfd8b2f14fc16593bc
Ilya Barashkov <ibarashkov@griddynamics.com>
2023-04-26 16:25:58 +0600

    fix: updated ioc-lite version

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -3,8 +3,15 @@ import { Logger } from '../services/logger';
 import { HTTP } from '../services/http';
 import { Users } from '../services/users';
 
+type IoCResources = {
+  users: typeof Users;
+  logger: typeof Logger;
+  http: typeof HTTP;
+  apiConfig: any;
+}
+
 export const createIoCContainer = () =>  {
-  const ioc = new IoCContainer();
+  const ioc = new IoCContainer<IoCResources>();
 
   ioc.registerClass('logger', Logger);
   ioc.registerClass('http', HTTP);
